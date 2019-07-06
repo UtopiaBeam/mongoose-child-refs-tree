3d39f790bbfcce279eecaa9c0b98048975159834
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2019-07-06 15:35:01 +0700

    test: init testing module

---
+++ test/index.spec.ts
@@ -0,0 +1,7 @@
+import ChildReferenceTreePlugin from '../lib';
+
+describe('Mongoose child reference tree plugin', () => {
+    it('Init plugin', () => {
+        expect(ChildReferenceTreePlugin).toBeDefined();
+    });
+});
