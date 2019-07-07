9a35638108e43865b1ad9d1b35ca46ea26fa68cc
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2019-07-07 17:36:37 +0700

    test: add sample tree as a base test case

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -19,10 +19,39 @@ describe('Mongoose child reference tree plugin function', () => {
     let Tree: mongoose.Model<TreeDocument>;
 
     beforeAll(async () => {
-        await mongoose.connect('mongodb://localhost:27017/seating-backend', { useNewUrlParser: true });
+        await mongoose.connect('mongodb://localhost:27017/mongoose-child-ref', { useNewUrlParser: true });
         treeSchema.plugin(ChildReferenceTreePlugin);
         Tree = mongoose.model('Tree', treeSchema);
     });
+
+    /**
+     * Initialize database with following structure
+     *    A
+     *  / | \
+     * B  C  D
+     *     \   
+     *      E 
+     */
+    beforeEach(async () => {
+        await Tree.deleteMany({});
+        let e = new Tree({ name: 'E' });
+        let b = new Tree({ name: 'B' });
+        let c = new Tree({ name: 'C', children: [e._id] });
+        let d = new Tree({ name: 'D' });
+        let a = new Tree({ name: 'A', children: [b._id, c._id, d._id] });
+        await a.save();
+        await b.save();
+        await c.save();
+        await d.save();
+        await e.save();
+    });
+
+    describe('Initial condition', () => {
+        it('should have 5 elements', async () => {
+            const elementCount = await Tree.count({});
+            expect(elementCount).toBe(5);
+        });
+    })
 
     describe('Database connection', () => {
         it('should connect to database', () => {
