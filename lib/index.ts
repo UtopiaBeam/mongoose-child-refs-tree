b2646872aa670b791b56b6e5ba83ac622553f1f8
UtopiaBeam <utopiabeam@gmail.com>
2019-07-07 13:59:43 +0700

    feat(remove): add pre-remove hook
    - add `children` field in schema
    - add cascade delete in `pre` hook

--- lib/index.ts
+++ lib/index.ts
@@ -1,5 +1,28 @@
-import { Schema } from 'mongoose';
+import { Schema, HookNextFunction, Collection } from 'mongoose';
+import { Options, ChildRefDocument } from './interface';
 
-function mongooseChildRef(schema: Schema, options) {}
+/**
+ * Main plugin method
+ * @param  {Schema} schema  Mongoose Schema
+ * @param  {Object} options [description]
+ */
+function mongooseChildRef(schema: Schema, options: Options): Schema {
+    const cascade: boolean = (options && options.cascade) || false;
+
+    schema.add({
+        children: [Schema.Types.ObjectId],
+    });
+
+    schema.pre<ChildRefDocument>('remove', function(next: HookNextFunction) {
+        if (!this.children) {
+            return next();
+        }
+        if (cascade === true) {
+            this.collection.deleteMany({ _id: { $in: this.children } }, next);
+        }
+    });
+
+    return schema;
+}
 
 export default mongooseChildRef;
